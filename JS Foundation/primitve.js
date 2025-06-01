7e11b16d6b9115184376f829248ee172914ecd13
Akashay Prakash Singh <112270350+AkashayP@users.noreply.github.com>
2025-06-02 00:17:03 +0530

    Create primitve.js

---
+++ JS Foundation/primitve.js
@@ -0,0 +1,23 @@
+let score = 120;
+
+console.log(typeof score);
+console.log(score);
+
+let username = 'Akashay';
+let afternoon = "Afternoon";
+
+let greetMessage = `Hii Good ${afternoon} ${username} !`;
+console.log(greetMessage);
+
+let value = `hey the value is ${2*8}.`
+console.log(value);
+
+let sm1 = Symbol("Akashay");
+let sm2 = Symbol("Akashay");
+console.log(sm1 == sm2);
+//Symbol always gives uniqueness no matter is the values are same this is the feature of symbol
+
+
+
+
+
